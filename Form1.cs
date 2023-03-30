ed425b0a15ba2848a2b450c5895a2dfa3fa771d9
QuenbKing <126509056+QuenbKing@users.noreply.github.com>
2023-03-30 22:19:43 +0500

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -21,35 +21,32 @@ namespace Game
         private static GameModel player;
         private static bool IsPress;
         KeyEventArgs Key;
-        BufferedGraphicsContext bufferedGraphicsContext;
-        BufferedGraphics bufferedGraphics;
         public Form1()
         {
-            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
-            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
-            DoubleBuffered = true;
+            DoubleBuffered= true;
             InitializeComponent();
-            Graphics graphics = this.CreateGraphics();
             PlayerImg = new Bitmap("E:\\GameForUniv\\Game\\ImagesForGame\\VinniPuh2.png");
             player = new GameModel(new Size(PlayerImg.Width / 2, PlayerImg.Height), Width / 2 - PlayerImg.Width / 2, Height / 2, PlayerImg);
-            bufferedGraphicsContext = new BufferedGraphicsContext();
-            bufferedGraphics = bufferedGraphicsContext.Allocate(graphics, new Rectangle(0, 0, Width, Height));
-            Background = new Bitmap("E:\\GameForUniv\\Game\\ImagesForGame\\oblaka.png");
+            Background = new Bitmap("E:\\GameForUniv\\Game\\ImagesForGame\\oblaka2.png");
             controller = new Controller(player);
+            SetBackground();
+            Paint += new PaintEventHandler(OnPaint);
             GetTimer_1();
             GetMove();
         }
 
-        private void GetBackground(Graphics gr)
+        private void OnPaint(object sender, PaintEventArgs e)
         {
-            var BackgroundNormally = new Bitmap(Background, new Size(Width, Height));
-            gr.DrawImage(BackgroundNormally, 0, 0, new Rectangle(new Point(0, 0), new Size(Width, Height)), GraphicsUnit.Pixel);
+            var gr = e.Graphics;
+            gr.DrawImage(player.playerImage, player.x, player.y, new Rectangle(new Point(125 * player.currFrame), player.size), GraphicsUnit.Pixel);
         }
 
-        private void Anim(Graphics gr)
+        private void SetBackground()
         {
-            gr.DrawImage(player.playerImage, player.x, player.y, new Rectangle(new Point(125 * player.currFrame), player.size), GraphicsUnit.Pixel);      
+            var BackgroundNormally = new Bitmap(Background, new Size(Width, Height));
+            BackgroundImage = BackgroundNormally;
         }
+
 
         private void GetMove()
         {
@@ -76,18 +73,11 @@ namespace Game
         }
         private void Timer1_Tick(object sender, EventArgs e)
         {
-            GetAnim();
             if (IsPress)
             {
                 controller.Move(sender, Key);
             }
-        }
-
-        private void GetAnim()
-        {
-            bufferedGraphics.Graphics.Clear(Color.White);
-            Anim(bufferedGraphics.Graphics);
-            bufferedGraphics.Render();
+            Invalidate();
         }
 
         static void Main()
