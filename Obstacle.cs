209315a2baaf2fdc65c522e5766935eb77b16ae8
QuenbKing <126509056+QuenbKing@users.noreply.github.com>
2023-04-02 21:12:26 +0500

    Add files via upload

---
+++ Obstacle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Obstacle
+    {
+        public static Image sprite;
+        public Transform transform;
+        public int sizeX;
+        public int sizeY;
+        public bool isTouchedByPlayer;
+
+        public Obstacle(PointF pos)
+        {
+            sprite = new Bitmap("E:\\GameForUniv\\Game\\ImagesForGame\\wall.png");
+            sizeX = 60;
+            sizeY = 12;
+            transform = new Transform(pos, new Size(sizeX, sizeY));
+            isTouchedByPlayer= false;
+        }
+
+        public void DrawObstacle(Graphics g)
+        {
+            g.DrawImage(sprite, transform.position.X, transform.position.Y, transform.size.Width, transform.size.Height);
+        }
+    }
+}
---
+++ ObstaclesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public static class ObstaclesController
+    {
+        public static List<Obstacle> obstacles;
+        public static int startPlatformPosY = 400;
+
+        public static void AddPlatform(PointF position)
+        {
+            Obstacle obs = new Obstacle(position);
+            obstacles.Add(obs);
+        }
+
+        public static void GenerateStartSequence()
+        {
+            Random r = new Random();
+            for(int i = 0; i < 3; i++)
+            {
+                int x = r.Next(0, 800);
+                int y = r.Next(0, 800);
+                startPlatformPosY -= y;
+                PointF pos = new PointF(x, startPlatformPosY);
+                Obstacle obstacle = new Obstacle(pos);
+                obstacles.Add(obstacle);
+            }
+        }
+
+        public static void GenerateRandomObstaclePos()
+        {
+            ClearObstacles();
+            Random r = new Random();
+            int x = r.Next(0, 800);
+            PointF pos = new Point(x, startPlatformPosY);
+            Obstacle obstacle = new Obstacle(pos);
+            obstacles.Add(obstacle);
+        }
+
+        public static void ClearObstacles()
+        {
+            for(int i = 0; i<obstacles.Count; i++)
+            {
+                if (obstacles[i].transform.position.Y >= 800)
+                    obstacles.RemoveAt(i);
+            }
+        }
+    }
+}
