79b056bdad447c36568843a67976df7ca42284ec
QuenbKing <126509056+QuenbKing@users.noreply.github.com>
2023-04-02 17:52:33 +0500

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -21,58 +21,96 @@ namespace Game
         private static GameModel player;
         private static bool IsPress;
         KeyEventArgs Key;
+        int _startDraw;
         public Form1()
         {
-            DoubleBuffered= true;
+            DoubleBuffered = true;
             InitializeComponent();
             PlayerImg = new Bitmap("E:\\GameForUniv\\Game\\ImagesForGame\\VinniPuh2.png");
+            Init();
+            this.Height = 800;
+            this.Width = 800;
             player = new GameModel(new Size(PlayerImg.Width / 2, PlayerImg.Height), Width / 2 - PlayerImg.Width / 2, Height / 2, PlayerImg);
             Background = new Bitmap("E:\\GameForUniv\\Game\\ImagesForGame\\oblaka2.png");
             controller = new Controller(player);
-            SetBackground();
+            StartDraw = 0;
             Paint += new PaintEventHandler(OnPaint);
-            GetTimer_1();
-            GetMove();
+            KeyDown += new KeyEventHandler(StartMove);
+            KeyUp += new KeyEventHandler(StopMove);
+
+            var timer1 = new Timer
+            {
+                Interval = 1
+            };
+            timer1.Tick += Timer1_Tick;
+            timer1.Start();
+
+            var timer2 = new Timer
+            {
+                Interval = 10
+            };
+            timer2.Tick += Timer2_Tick;
+            timer2.Start();
+        }
+        int StartDraw
+        {
+            get => _startDraw;
+            set
+            {
+                _startDraw = value;
+                if (_startDraw > 0) _startDraw -= Background.Height;
+            }
+        }
+
+        public void Init()
+        {
+            ObstaclesController.obstacles = new List<Obstacle>();
+            ObstaclesController.AddPlatform(new PointF(100, 400));
+            ObstaclesController.startPlatformPosY = 400;
+            ObstaclesController.GenerateStartSequence();
         }
 
         private void OnPaint(object sender, PaintEventArgs e)
         {
             var gr = e.Graphics;
+            for(int i = 0; i < 3; i++)
+            {
+                gr.DrawImage(Background, 0, StartDraw + Background.Height*i);
+            }
             gr.DrawImage(player.playerImage, player.x, player.y, new Rectangle(new Point(125 * player.currFrame), player.size), GraphicsUnit.Pixel);
-        }
-
-        private void SetBackground()
-        {
-            var BackgroundNormally = new Bitmap(Background, new Size(Width, Height));
-            BackgroundImage = BackgroundNormally;
-        }
-
-
-        private void GetMove()
-        {
-            KeyDown += (s, e) =>
+            if(ObstaclesController.obstacles.Count > 0)
             {
-                IsPress = true;
-                Key = e;
-            };
-            KeyUp += (s, e) =>
-            {
-                if (Key.KeyCode == e.KeyCode)
-                    IsPress = false;
-            };
+                for (int i = 0; i < ObstaclesController.obstacles.Count; i++)
+                    ObstaclesController.obstacles[i].DrawObstacle(gr);
+            }
         }
 
-        private void GetTimer_1()
+        private void Follow()
         {
-            var timer = new Timer
+            int offset = 400 - player.y;
+            for(int i = 0; i<ObstaclesController.obstacles.Count; i++)
             {
-                Interval = 1
-            };
-            timer.Tick += Timer1_Tick;
-            timer.Start();
+                var obstacle = ObstaclesController.obstacles[i];
+                obstacle.transform.position.Y += offset;
+            }
         }
+
+
+        private void StopMove(object sender, KeyEventArgs e)
+        {
+            if (Key.KeyCode == e.KeyCode)
+                IsPress = false;
+        }
+
+        private void StartMove(object sender, KeyEventArgs e)
+        {
+            IsPress = true;
+            Key = e;
+        }
+
         private void Timer1_Tick(object sender, EventArgs e)
         {
+            StartDraw += 3;
             if (IsPress)
             {
                 controller.Move(sender, Key);
@@ -80,39 +118,16 @@ namespace Game
             Invalidate();
         }
 
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            Follow();
+            ObstaclesController.GenerateRandomObstaclePos();
+            Invalidate();
+        }
+
         static void Main()
         {
             Application.Run(new Form1());
-        }
-    }
-
-    class Controller
-    {
-        public GameModel player;
-        public Controller(GameModel model)
-        {
-            player = model;
-        }
-
-        public void Move(object sender, KeyEventArgs e)
-        {
-            switch (e.KeyCode.ToString())
-            {
-                case "D":
-                    player.MoveRight();
-                    player.currFrame = 1;
-                    break;
-                case "A":
-                    player.MoveLeft();
-                    player.currFrame = 0;
-                    break;
-                case "W":
-                    player.MoveUp();
-                    break;
-                case "S":
-                    player.MoveDown();
-                    break;
-            }
         }
     }
 }
